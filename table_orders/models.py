dca10a1547f761f19e49e03ec9136ce9a886877e
R.S.Muruganandham <rs.muruganandham03@gmail.com>
2026-02-23 11:39:20 +0530

    added all

--- table_orders/models.py
+++ table_orders/models.py
@@ -9,21 +9,25 @@ class DiningTable(BaseModel):
     seating_capacity = models.IntegerField()
     is_occupied = models.BooleanField(default=False)
     
-
+    def __str__(self):
+        return str(self.table_number) + "===="+str(self.seating_capacity)
+    
 class Customer(BaseModel):
     name = models.CharField(max_length=100,null=True,blank=True)
     phone_number = models.CharField(max_length=100,null=True,blank=True)
     email = models.CharField(max_length=150,null=True,blank=True)
     
-    # def __str__(self):
-    #     return self.name + "===="+str(self.phone_number)
+    def __str__(self):
+        return str(self.name) + "===="+str(self.phone_number)
     
 class Order(BaseModel):
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
     table= models.ForeignKey(DiningTable,on_delete=models.CASCADE)
     menu_items = models.ManyToManyField(MenuItem)
     order_time = models.DateTimeField(auto_now_add=True)
-    is_completed = models.BooleanField(default=True)
-    total_amount = models.IntegerField() 
+    is_completed = models.BooleanField(default=False)
+    total_amount = models.IntegerField(default=0)
     
+    def __str__(self):
+        return str(self.customer.name) + "===="+str(self.table.table_number)
     
